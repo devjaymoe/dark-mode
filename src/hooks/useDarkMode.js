6f27869c92372be7771bfe3489dcb3c4ebeb457d
Devin_Moore <d.j.moore209@gmail.com>
2020-04-08 19:28:15 -0400

    slight refactor of the dark mode hook to make it more clear

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,14 +2,14 @@ import { useEffect } from 'react'
 import { useLocalStorage } from './useLocalStorage';
 
 export const useDarkMode = () => {
-    const [darkMode, setDarkMode] = useLocalStorage(false)
+    const [darkMode, setDarkMode] = useLocalStorage('Dark Mode', false)
 
     useEffect(()=>{
 
         if (darkMode === true){
-            document.querySelector("body").classList.toggle('dark-mode') 
+            document.body.classList.toggle('dark-mode') 
         } else {
-            document.querySelector("body").classList.remove('dark-mode')
+            document.body.classList.remove('dark-mode')
         }
 
     }, [darkMode])
